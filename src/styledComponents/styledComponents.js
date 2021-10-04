23849fcae7d20d7835c6d1461a16c62c70bd8e50
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-10-04 10:33:26 -0500

    Login page created

--- src/styledComponents/styledComponents.js
+++ src/styledComponents/styledComponents.js
@@ -2,7 +2,12 @@ import styled from 'styled-components';
 import { InputBase } from '@material-ui/core';
 
 export const NetflixInput = styled(InputBase)`
-	
+	z-index: ;
+	background: #fff;
+	padding: 25.5px;
+	height: 30px;
+	border-radius: 5px;
+	margin-top: -2px;
 `;
 
 export const NetflixButton = styled.button`
