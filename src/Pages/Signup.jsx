20e54689c8d1df6139e12c5ba80f6d91b6d7c89c
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-10-06 20:39:48 -0500

    Starting trying to learn firebase

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/styles';
-import { Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Typography, makeStyles } from '@material-ui/core';
 import {
 	NetflixButton,
 	NetflixInput,
 } from '../styledComponents/styledComponents';
 
+
 const Signup = () => {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
 	const classes = useStyles();
+
+	const register = event => {
+		event.preventDefault();
+	};
+	const signIn = event => {
+		event.preventDefault();
+		auth.createUserWithEmailPassword(email, password)
+		.then()
+		.catch()
+	};
+
 	return (
 		<div className={classes.root}>
 			<Typography variant="h5" align="left">
 				Sign In
 			</Typography>
 			<form className={classes.form}>
-				<NetflixInput />
-				<NetflixInput />
-				<NetflixButton wide="medium" radius>
+				<NetflixInput
+					value={email}
+					onChange={event => setEmail(event.target.value)}
+					placeholder="Email"
+					className={classes.email}
+				/>
+				<NetflixInput
+					value={password}
+					onChange={event => setPassword(event.target.value)}
+					placeholder="Password"
+					className={classes.password}
+				/>
+				<NetflixButton wide="medium" radius type="submit" onClick={signIn}>
 					Sign In
 				</NetflixButton>
+				<Typography variant="subtitle2" className={classes.subtitle}>
+					New to Netflix ?{' '}
+					<span onClick={register} className={classes.signupLink}>
+						Sign Up now.{' '}
+					</span>
+				</Typography>
 			</form>
 		</div>
 	);
@@ -27,5 +56,33 @@ const Signup = () => {
 export default Signup;
 
 const useStyles = makeStyles(theme => ({
-	root: {},
+	root: {
+		maxWidth: '350px',
+		width: '20rem',
+		height: '25rem',
+		background: 'rgba(0,0,0,0.65)',
+		display: 'flex',
+		flexDirection: 'column',
+		justifyContent: 'space-evenly',
+		alignItems: 'center',
+	},
+	form: {
+		width: '80%',
+	},
+	email: {
+		marginBottom: theme.spacing(2),
+	},
+	password: {
+		marginBottom: theme.spacing(2),
+	},
+	subtitle: {
+		marginTop: theme.spacing(2),
+	},
+	signupLink: {
+		cursor: 'pointer',
+		color: 'red',
+		'&:hover': {
+			textDecoration: 'underline',
+		},
+	},
 }));
---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyAKMmgQEMAPS2St8bZUdGOVi2MNSD0Xk48',
+	authDomain: 'netflix-replica-d3b5a.firebaseapp.com',
+	projectId: 'netflix-replica-d3b5a',
+	storageBucket: 'netflix-replica-d3b5a.appspot.com',
+	messagingSenderId: '364766051296',
+	appId: '1:364766051296:web:46b9d543c26f6076f8c985',
+};
+
+//! Probable error
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
